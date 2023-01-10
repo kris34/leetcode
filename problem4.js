1aa7266393be00f26fb52179b45988a026395bc7
Kristian Lalov <lalov.kr@gmail.com>
2023-01-11 00:05:00 +0200

    changed the solution

--- problem4.js
+++ problem4.js
@@ -1,6 +1,5 @@
 var interpret = function (command) {
-  let result1 = command.replaceAll('()', 'o');
-
-  return result1.replaceAll('(al)', 'al')
+  command = command.replaceAll('()', 'o');
+  return command.replaceAll('(al)', 'al');
 };
-interpret('G()(al)');
+console.log(interpret('G()(al)'));
