31bfe0e8c63c68c45f8ac90b892ac61a6c05b2e6
Kristian Lalov <lalov.kr@gmail.com>
2023-01-11 00:09:45 +0200

    added a forLoop algorithm

--- problem4.js
+++ problem4.js
@@ -1,5 +1,23 @@
-var interpret = function (command) {
+/* var interpret = function (command) {
   command = command.replaceAll('()', 'o');
   return command.replaceAll('(al)', 'al');
 };
-console.log(interpret('G()(al)'));
+console.log(interpret('G()(al)')); */
+
+var interpret = function (command) {
+  let result = '';
+
+  for (let i = 0; i < command.length; i++) {
+    if (command[i] == 'G') {
+      result += 'G';
+    } else if (command[i + 1] == ')') {
+      result += 'o';
+      i += 1;
+    } else {
+      result += 'al';
+      i += 3;
+    }
+  }
+  return result
+};
+interpret('G()(al)');
