ef053af711ec5d941fb5b10fbfb5022fe2ee898a
Kristian Lalov <lalov.kr@gmail.com>
2023-02-18 21:04:04 +0200

    solved problem 2325

---
+++ problem7.js
@@ -0,0 +1,31 @@
+var decodeMessage = function (key, message) {
+  let charCode = 'a'.charCodeAt(0);
+  const map = new Map();
+
+  for (let i = 0; i < key.length; i++) {
+    let char = key[i];
+
+    if (map[char] == undefined && char != ' ') {
+      map[char] = String.fromCharCode(charCode);
+      charCode += 1;
+    }
+  }
+  let result = ""
+
+  for (let i = 0; i < message.length; i++) {
+    let char = message[i];
+
+    if (char == ' ') {
+      result += char;
+    } else {
+      result += map[char];
+    }
+  }
+
+  console.log(result);
+};
+
+decodeMessage(
+  'the quick brown fox jumps over the lazy dog',
+  'vkbs bs t suepuv'
+);
