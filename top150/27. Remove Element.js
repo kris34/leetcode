a487cdac05d56c313e57bca388e774a3dda9e9b5
Kristian Lalov <lalov.kr@gmail.com>
2023-05-16 16:52:10 +0300

    solved problem 27

---
+++ top150/27. Remove Element.js
@@ -0,0 +1,13 @@
+var removeElement = function (nums, val) {
+  let newNums = nums.filter((el) => el != val);
+
+  for (let i = 0; i < newNums.length; i++) {
+    nums[i] = newNums[i];
+  }
+
+  return newNums.length;
+};
+
+let k = removeElement([3, 3, 3, 3, 2, 2, 3], 3);
+
+console.log(k);
--- top150/88. Merge Sorted Array.js
+++ top150/88. Merge Sorted Array.js
@@ -1,12 +1,29 @@
+/* You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n,
+ representing the number of elements in nums1 and nums2 respectively.
+
+Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+The final sorted array should not be returned by the function, but instead be stored inside the array nums1.
+ To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged,
+  and the last n elements are set to 0 and should be ignored. nums2 has a length of n. */
+
 function merge(nums1, m, nums2, n) {
-  let counter = 0;
-  
+  let nums2Index = 0;
+
   for (let i = m; i < m + n; i++) {
-    nums1[i] += nums2[counter];
-    counter++;
+    nums1[i] += nums2[nums2Index];
+    nums2Index++;
   }
 
   return nums1.sort((a, b) => a - b);
 }
 
 console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3));
+
+/* Since always the last n elements are going to be zero,
+we dont care about the first m elements so we start the loop from
+m onwards. The loop continues untill m + n becasue we have to replace the last n elements of nums1 with all elements from nums2.
+Since the last n elements of nums1 will always be 0 we can just do .
+The counter comes into play because we have to start from index 0 of nums2
+and we just increment it by 1 every time untills the loop is over. 
+Once its done we just have to perform a basic sorting operation. */
